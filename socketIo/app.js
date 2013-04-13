7500d5827bcb0f1e613351e54acec14331c23428
Bader <bn44@njit.edu>
2013-04-13 05:42:50 +0000

    implementing example of a chat using the SocketIO

--- socketIo/app.js
+++ socketIo/app.js
@@ -1,26 +1,27 @@
+var fs = require('fs'),
+    http = require('http'),
+    sio = require('socket.io');
 
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-
-app.listen(3003);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-type': 'text/html'});
+  res.end(fs.readFileSync('./index.html'));
+});
+server.listen(3698, function() {
+  console.log('Server listening at http://bader.invengion.org:3698/');
+});
+// Attach the socket.io server
+io = sio.listen(server);
+// store messages
+var messages = [];
+// Define a message handler
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data)
+  socket.on('message', function (msg) {
+    console.log('Received: ', msg);
+    messages.push(msg);
+    socket.broadcast.emit('message', msg);
   });
+  // send messages to new clients
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  })
 });
